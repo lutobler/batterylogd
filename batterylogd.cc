3a5563ed8522e496428189820cfda1cf851ef562
Lukas Tobler <luk4s.tobler@gmail.com>
2018-01-18 20:09:04 +0100

    C++ify the code
    Might as well do object orientation, polymorphism and smart pointers if
    we already have C++.

--- batterylogd.cc
+++ batterylogd.cc
@@ -1,4 +1,4 @@
-/*
+/**
  * Copyright (c) 2017, Lukas Tobler
  * GNU General Public License v3.0
  * (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
@@ -20,6 +20,8 @@
 #include <ctime>
 #include <mutex>
 #include <condition_variable>
+#include <memory>
+#include <assert.h>
 
 static const std::string kBatterylogdVersion("0.1");
 static const char *kUsage =
@@ -28,27 +30,23 @@ static const char *kUsage =
 "  -v   --version       Print version information and exit.\n"
 "  -i   --interval      Sampling interval. Defaults to 30 seconds.\n"
 "  -b   --battery       Path to a battery in sysfs. Argument can be specified\n"
-"                       multiple times. If omitted entirely, automatic battery\n"
-"                       detection will be enabled.\n"
+"                       multiple times. If omitted entirely, automatic detection\n"
+"                       will be enabled.\n"
 "  -L   --backlight     Add entries for the display backlight to the log.\n"
-"                       Fields for 'max_brightness' and 'current_brightness'\n"
-"                       will be appended to the logged battery data.\n"
+"                       Argument can be specified multiple times. If omitted\n"
+"                       entirely, automatic detection will be enabled.\n"
 "  -l   --log           Path to log file. Defaults to $HOME/batterylogd.log.\n\n";
 
-static const std::string kDefaultLogfile("batterylogd.log");
+/* standard device paths */
 static const std::string kBatterySysfs("/sys/class/power_supply/");
+static const std::string kBacklightSysfs("/sys/class/backlight/");
+
+/* defaults */
+static const std::string kDefaultLogfile("batterylogd.log");
 static const int kDefaultSampleInterval = 60;
 
 /* set by signal handler to indicate the program should terminate */
 static volatile std::sig_atomic_t sig_terminated = false;
-
-/* strip a trailing slash from a string if needed */
-static std::string strip_traling_slash(std::string str) {
-    if (str.back() == '/') {
-        return str.substr(0, str.size() - 1);
-    }
-    return str;
-}
 
 /* a timer that can be interrupted prematurely */
 struct KillableTimer {
@@ -72,87 +70,82 @@ public:
     }
 };
 
-struct DisplayBacklightStat {
+/* a data point that represents a file in the file system */
+class FSDataPoint {
+private:
+    std::string path_;
+    std::ifstream file_;
+    std::string data_;
+
 public:
-    std::string path;
-    std::ifstream f_brightness;
-    std::ifstream f_max_brightness;
-
-    std::string brightness;
-    std::string max_brightness;
-
-    DisplayBacklightStat(std::string& path_) {
-        path = strip_traling_slash(path_);
-    }
-
-    ~DisplayBacklightStat() {
-        f_brightness.close();
-        f_max_brightness.close();
-    }
-
-    void sample() {
-        std::getline(f_brightness, brightness);
-        std::getline(f_max_brightness, max_brightness);
-
-        /* getline will put the file cursor to the end so we reset it */
-        f_brightness.clear();
-        f_brightness.seekg(0, std::ios::beg);
-        f_max_brightness.clear();
-        f_max_brightness.seekg(0, std::ios::beg);
-    }
+    FSDataPoint(std::string path) : path_(path) {}
 
     bool initialize() {
-        f_brightness.open(path + "/brightness");
-        f_max_brightness.open(path + "/max_brightness");
-        return f_brightness && f_max_brightness;
-    }
-
-    std::string build_log_string() {
-        return brightness + "," + max_brightness;
-    }
-};
-
-struct BatteryStat {
-public:
-    std::string path;
-    std::string data;
-
-    BatteryStat(std::string& base_path, std::string& fname) {
-        path = strip_traling_slash(base_path) + "/" + fname;
+        file_.open(path_);
+        return file_.good();
     }
 
     void sample() {
-        std::ifstream file;
-        file.open(path);
-        if (file) {
-            std::getline(file, data);
-        }
-        file.close();
+        std::getline(file_, data_);
+        file_.clear();
+        file_.seekg(0, std::ios::beg);
     }
+
+    const std::string& data() const { return data_; }
 };
 
-struct Battery {
-private:
-    bool is_battery_dir(std::string& path) {
-        std::ifstream f_type;
-        f_type.open(path + "/type");
-        if (!f_type) {
-            return false;
-        }
-        std::string file_content;
-        std::getline(f_type, file_content);
-        f_type.close();
-        return file_content == "Battery";
+/* abstract container for a collection of data points */
+class DataCollection {
+protected:
+    std::vector<FSDataPoint> data_points_;
+    std::string sysfs_path_;
+    std::string name_;
+    std::string type_;
+
+    const std::string name_from_path(std::string& path) const {
+        return std::string(basename((char *) path.c_str()));
     }
 
 public:
-    std::string sysfs_path;
-    std::string name;
-    std::vector<BatteryStat> battery_stats;
+    DataCollection(std::string sysfs_path) {
+        /* strip a trailing slash from path if needed */
+        if (sysfs_path.back() == '/') {
+            sysfs_path_ = sysfs_path.substr(0, sysfs_path.size() - 1);
+        } else {
+            sysfs_path_ = sysfs_path;
+        }
+        name_ = name_from_path(sysfs_path);
+    }
 
-    Battery(std::string& sysfs_path_) {
-        sysfs_path = strip_traling_slash(sysfs_path_);
-        name = basename((char *) sysfs_path.c_str());
+    virtual bool initialize() = 0;
+
+    void sample_all() {
+        for (FSDataPoint& dp : data_points_) {
+            dp.sample();
+        }
+    }
+
+    const std::vector<std::string> data_vector() {
+        std::vector<std::string> dv;
+        dv.reserve(data_points_.size());
+        for (FSDataPoint& dp : data_points_) {
+            dv.push_back(dp.data());
+        }
+        return dv;
+    }
+
+    const std::string& name() const { return name_; }
+    const std::string& type() const { return type_; }
+};
+
+/* a data collection that models a battery */
+class BatteryDataCollection : public DataCollection {
+public:
+    BatteryDataCollection(std::string sysfs_path)
+        : DataCollection(sysfs_path) {}
+
+    bool initialize() {
+        type_ = "battery";
         std::vector<std::string> files {
             "capacity",
             "cycle_count",
@@ -165,60 +158,149 @@ public:
             "voltage_min_design",
             "voltage_now",
         };
+        data_points_.reserve(files.size());
         for (std::string& s : files) {
-            battery_stats.push_back(BatteryStat(sysfs_path, s));
+            FSDataPoint fdp(sysfs_path_ + "/" + s);
+            if (!fdp.initialize()) {
+                return false;
+            }
+            data_points_.push_back(std::move(fdp));
         }
-    }
-
-    bool initialize() {
-        return (is_battery_dir(sysfs_path));
-    }
-
-    void sample_all() {
-        for (BatteryStat& b : battery_stats) {
-            b.sample();
-        }
-    }
-
-    std::string build_log_string() {
-        std::stringstream ss;
-        std::time_t t = std::chrono::system_clock::to_time_t(
-                std::chrono::system_clock::now());
-
-        ss << name << "," << std::put_time(std::localtime(&t), "%FT%TZ");
-        for (BatteryStat& b : battery_stats) {
-            ss << "," << b.data;
-        }
-        return ss.str();
-    }
-
-    std::vector<std::string> data_vector() {
-        std::vector<std::string> v(battery_stats.size());
-        for (BatteryStat& b : battery_stats) {
-            v.push_back(b.data);
-        }
-        return v;
+        return true;
     }
 };
 
-/* try to find batteries in /sys/class/power_supply/ */
-static void detect_batteries(std::vector<Battery>& batteries) {
-    DIR *dir;
-    struct dirent *ent;
-    if ((dir = opendir(kBatterySysfs.c_str())) != nullptr) {
-        while ((ent = readdir(dir)) != nullptr) {
-            std::ifstream f_type;
-            std::string path = kBatterySysfs + ent->d_name;
-            Battery battery(path);
-            if (battery.initialize()) {
-                batteries.push_back(battery);
-                std::cout << "Added battery " << battery.name
-                          << std::endl;
+class BacklightDataCollection : public DataCollection {
+public:
+    BacklightDataCollection(std::string sysfs_path)
+        : DataCollection(sysfs_path) {}
+
+    bool initialize() {
+        type_ = "backlight";
+        std::vector<std::string> files { "brightness", "max_brightness" };
+        data_points_.reserve(files.size());
+        for (std::string& s : files) {
+            FSDataPoint fdp(sysfs_path_ + "/" + s);
+            if (!fdp.initialize()) {
+                return false;
             }
+            data_points_.push_back(std::move(fdp));
+        }
+        return true;
+    }
+};
+
+class LogBuilder {
+private:
+    std::vector<std::shared_ptr<DataCollection>> data_collections_;
+    std::ofstream log_file_;
+
+public:
+    LogBuilder(std::vector<std::shared_ptr<DataCollection>> data_collections,
+               std::ofstream log_file)
+        : data_collections_(data_collections), log_file_(std::move(log_file)) {}
+
+    void write() {
+        for (std::shared_ptr<DataCollection>& dc : data_collections_) {
+            std::stringstream ss;
+            std::vector<std::string> dv = dc.get()->data_vector();
+            std::time_t t = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+
+            for (std::string& s : dv) {
+                ss << "," << s;
+            }
+
+            log_file_ << dc.get()->type()
+                      << "," << dc.get()->name()
+                      << "," << std::put_time(std::localtime(&t), "%FT%TZ")
+                      << ss.str() << std::endl;
         }
     }
-    closedir(dir);
-}
+};
+
+template <class T>
+class DeviceDetector {
+private:
+    std::string base_path_;
+    std::string detect_file_;
+    std::string detect_string_;
+
+    std::vector<std::shared_ptr<DataCollection>> auto_detect_() {
+        std::vector<std::shared_ptr<DataCollection>> detected;
+        DIR *dir;
+        struct dirent *ent;
+
+        /* open the base path */
+        if ((dir = opendir(base_path_.c_str())) != nullptr) {
+
+            /* loop through the contents of the base path */
+            while ((ent = readdir(dir)) != nullptr) {
+                std::ifstream f_detect;
+                std::string path = base_path_ + ent->d_name;
+                f_detect.open(path + "/" + detect_file_);
+
+                /* read and check the detection file */
+                if (f_detect.good()) {
+                    std::string f_content;
+                    std::getline(f_detect, f_content);
+                    if (f_content == detect_string_) {
+                        std::shared_ptr<DataCollection> dc_ptr(new T(path));
+                        if (dc_ptr->initialize()) {
+                            detected.push_back(dc_ptr);
+                        }
+                    }
+                }
+            }
+            closedir(dir);
+        }
+        return detected;
+    }
+
+public:
+    DeviceDetector(std::string base_path, std::string detect_file, std::string detect_string)
+        : base_path_(base_path), detect_file_(detect_file), detect_string_(detect_string) {
+            assert(base_path_.back() == '/');
+        }
+
+    /*
+     * Construct data collection from command line and perform auto-detection
+     * if none are given.
+     */
+    bool detect_from_cmdline(std::vector<std::shared_ptr<DataCollection>>& data_collections,
+                             std::vector<std::string>& args) {
+
+        /* no cmdline arguments given, auto-detect */
+        std::vector<std::shared_ptr<DataCollection>> detected;
+        if (args.size() == 0) {
+            detected = auto_detect_();
+            if (detected.size() == 0) {
+                std::cerr << "No device in " << base_path_
+                          << " found. Provide -b argument." << std::endl;
+                return false;
+            }
+
+        /* use cmdline arguements */
+        } else {
+            for (std::string& s : args) {
+                std::shared_ptr<DataCollection> t_ptr(new T(s));
+                if (t_ptr->initialize()) {
+                    detected.push_back(t_ptr);
+                }
+            }
+        }
+
+        /* log configured batteries to stdout */
+        for (std::shared_ptr<DataCollection>& dc : detected) {
+            std::cout << "Added device " << dc.get()->name() << std::endl;
+        }
+
+        /* merge vectors */
+        data_collections.reserve(data_collections.size() + detected.size());
+        data_collections.insert(data_collections.end(), detected.begin(), detected.end());
+
+        return true;
+    }
+};
 
 static void signal_handler(int signo) {
     (void) signo;
@@ -239,13 +321,12 @@ int main(int argc, char **argv) {
     };
 
     std::vector<std::string> cmdline_batteries;
-    std::string cmdline_backlight;
-    bool backlight_logging_enabled = false;
+    std::vector<std::string> cmdline_backlights;
     std::string cmdline_logfile;
     int cmdline_interval = 0;
-    int c;
 
     /* parse cmdline arguemnts */
+    int c;
     while (true) {
         int option_index = 0;
         c = getopt_long(argc, argv, optstring, long_options, &option_index);
@@ -260,9 +341,7 @@ int main(int argc, char **argv) {
             std::exit(EXIT_SUCCESS);
             break;
         case 'v':
-            std::cout << "batterylogd: version "
-                      << kBatterylogdVersion
-                      << std::endl;
+            std::cout << "batterylogd: version " << kBatterylogdVersion << std::endl;
             std::exit(EXIT_SUCCESS);
             break;
         case 'i':
@@ -276,8 +355,7 @@ int main(int argc, char **argv) {
             cmdline_batteries.push_back(std::string(optarg));
             break;
         case 'L':
-            cmdline_backlight = std::string(optarg);
-            backlight_logging_enabled = true;
+            cmdline_backlights.push_back(std::string(optarg));
             break;
         case 'l':
             cmdline_logfile = std::string(optarg);
@@ -290,56 +368,31 @@ int main(int argc, char **argv) {
         }
     }
 
-    /* set sample interval */
-    int sample_interval;
-    if (cmdline_interval == 0) {
-        sample_interval = kDefaultSampleInterval;
-    } else {
-        sample_interval = cmdline_interval;
-    }
+    /* data collections to observe, i.e. batteries and backlights */
+    std::vector<std::shared_ptr<DataCollection>> data_collections;
 
-    /* initialize batteries */
-    std::vector<Battery> batteries;
-    if (cmdline_batteries.size() == 0) {
-        detect_batteries(batteries);
-        if (batteries.size() == 0) {
-            std::cerr << "No batteries found. Provide -b argument."
-                      << std::endl;
-            std::exit(EXIT_FAILURE);
-        }
-    } else {
-        for (std::string& s : cmdline_batteries) {
-            Battery battery(s);
-            if (battery.initialize()) {
-                batteries.push_back(battery);
-                std::cout << "Added battery " << battery.name << std::endl;
-            }
-        }
+    DeviceDetector<BatteryDataCollection> battery_detector(kBatterySysfs, "type", "Battery");
+    DeviceDetector<BacklightDataCollection> backlight_detector(kBacklightSysfs, "type", "raw");
+    if (!battery_detector.detect_from_cmdline(data_collections, cmdline_batteries)) {
+        std::exit(EXIT_FAILURE);
     }
-
-    /* initialize backlight */
-    DisplayBacklightStat backlight(cmdline_backlight);
-    if (backlight_logging_enabled) {
-        if (!backlight.initialize()) {
-            std::cout << "Invalid backlight given" << std::endl;
-            std::exit(EXIT_FAILURE);
-        }
-        std::cout << "Added backlight " << cmdline_backlight << std::endl;
+    if (!backlight_detector.detect_from_cmdline(data_collections, cmdline_backlights)){
+        std::exit(EXIT_FAILURE);
     }
 
     /* initialize log file */
-    std::ofstream log_file;
     if (cmdline_logfile.empty()) {
-        cmdline_logfile = std::string(std::getenv("HOME"))
-                          + "/" + kDefaultLogfile;
+        cmdline_logfile = std::string(std::getenv("HOME")) + "/" + kDefaultLogfile;
     }
-    std::cout << "Log file: " << cmdline_logfile << std::endl;
 
-    log_file.open(cmdline_logfile);
-    if (!log_file) {
+    std::ofstream log_file;
+    log_file.open(cmdline_logfile, std::ios_base::app);
+    if (!log_file.good()) {
         std::cerr << "Could not open log file" << std::endl;
         std::exit(EXIT_FAILURE);
     }
+    std::cout << "Log file: " << cmdline_logfile << std::endl;
+    LogBuilder log_builder(data_collections, std::move(log_file));
 
     /* register signal handlers */
     if (signal(SIGINT, signal_handler) == SIG_ERR ||
@@ -347,6 +400,10 @@ int main(int argc, char **argv) {
         std::cerr << "Failed to regiser signal handler" << std::endl;
         std::exit(EXIT_FAILURE);
     }
+
+    /* set sample interval */
+    int sample_interval = (cmdline_interval == 0) ? kDefaultSampleInterval
+                                                  : cmdline_interval;
 
     /*
      * Use a signal handler thread because operations on condition variables
@@ -365,15 +422,10 @@ int main(int argc, char **argv) {
     std::thread main_loop([&] (void) {
         std::chrono::seconds interval(sample_interval);
         while (true) {
-            for (Battery& b : batteries) {
-                b.sample_all();
-                std::string log_line = b.build_log_string();
-                if (backlight_logging_enabled) {
-                    backlight.sample();
-                    log_line += backlight.build_log_string();
-                }
-                log_file << log_line << std::endl;
+            for (std::shared_ptr<DataCollection>& dc : data_collections) {
+                dc.get()->sample_all();
             }
+            log_builder.write();
 
             if (!timer.wait_for(interval)) {
                 break;
